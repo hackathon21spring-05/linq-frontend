4841b64fe4d8160f65999151d94e22c7aca1f603
trasta <trasta298@gmail.com>
2021-06-15 03:12:14 +0900

    add lint

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>
   export default component
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import WindiCSS from "vite-plugin-windicss"
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
