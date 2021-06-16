c0a469a0915ce8661676c9723adba7a73a9b927c
trasta <trasta298@gmail.com>
2021-06-17 00:23:15 +0900

    add router

---
+++ src/pages/Add.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Add</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Add'
+})
+</script>
---
+++ src/pages/Entry.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Entry {{ url }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  name: 'Entry',
+  setup() {
+    const route = useRoute()
+    const url = route.fullPath.slice(7)
+    return {
+      url
+    }
+  }
+})
+</script>
