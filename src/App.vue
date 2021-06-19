50498ebbb228db870cad1feeb33a0ab06d3795f6
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-20 04:01:12 +0900

    不要部分delete

--- src/App.vue
+++ src/App.vue
@@ -27,22 +27,6 @@ export default defineComponent({
     const store = useStore()
     const fetchedMe = computed(() => store.state.me !== null)
 
-    // onBeforeMount(() => {
-    //   if (fetchedMe.value) return
-    //   store.dispatch.fetchMe().catch(async (error) => {
-    //     if (error.response.status === 401) {
-    //       const { data } = await apis.getGeneratedCode()
-    //       const authorizationEndpointUrl = new URL('https://q.trap.jp/api/v3/oauth2/authorize')
-    //       authorizationEndpointUrl.search = new URLSearchParams({
-    //         response_type: 'code',
-    //         client_id: data.clientID,
-    //         code_challenge: data.codeChallenge,
-    //         code_challenge_method: data.codeChallengeMethod
-    //       }).toString()
-    //       window.location.assign(authorizationEndpointUrl.toString())
-    //     }
-    //   })
-    // })
     return {
       fetchedMe
     }
