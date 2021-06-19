a08022dbf54fad59aa75d502310fab2219c58b53
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-19 17:17:22 +0900

    検索のAPIを適用

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -16,8 +16,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watchEffect } from 'vue'
 import useQuery from '/@/use/query'
+import apis, {EntryDetail} from '/@/lib/apis'
 import EntryTile from '/@/components/EntryTile.vue'
 
 export default defineComponent({
@@ -29,39 +30,12 @@ export default defineComponent({
     const tag = useQuery('tag')
     // const title = useQuery('title')
     // const text = useQuery('text')
-    const searchEntries = [
-      {
-        'url': 'https://zenn.dev/',
-        'title': '加藤恵 「どうかな？わたしは、あなたが望む、メインヒロインに、なれたかな？」「どうかな？わたしは、あなたが望む、メインヒロインに、なれたかな？」',
-        'captiopn': '豊ヶ崎学園に通う高校2年生で、同人サークル『Blessing software』のメインヒロイン(役割不明)。のちにサークル副代表を兼任。目鼻立ちが整っていて、可愛いと綺麗が中途半端に同居した容姿。白いベレー帽とスマホがトレードマーク。',
-        'thumbnail': 'https://images-na.ssl-images-amazon.com/images/I/61kNxS8EZtL._AC_SY355_.jpg',
-        'tags': [
-          'kato', 'megumi', 'kashiwagi', 'eriri',
-          'kasumigaoka', 'utaha', 'hyodo', 'michiru', 'hashima', 'izumi'
-        ],
-        'isBookmark': false
-      },
-      {
-        'url': 'https://zenn.dev/',
-        'title': '加藤恵',
-        'captiopn': '豊ヶ崎学園に通う高校2年生で、同人サークル『Blessing software』のメインヒロイン(役割不明)。のちにサークル副代表を兼任。目鼻立ちが整っていて、可愛いと綺麗が中途半端に同居した容姿。白いベレー帽とスマホがトレードマーク。',
-        'thumbnail': 'https://saenai-movie.com/megumi_birthday_fine/present/assets/img/top/img_main.jpg',
-        'tags': [
-          'kato', 'megumi'
-        ],
-        'isBookmark': true
-      },
-      {
-        'url': 'https://zenn.dev/',
-        'title': 'test3',
-        'captiopn': 'oha',
-        'thumbnail': 'https://ogre.natalie.mu/media/pp/static/comic/saenai-heroine02/photo01s.jpg',
-        'tags': [
-          'kato', 'megumi'
-        ],
-        'isBookmark': false
-      }
-    ]
+    
+    const searchEntries = ref<EntryDetail[]>()
+    watchEffect(async () => {
+      const res = await apis.getSearchEntry(tag.value)
+      searchEntries.value = res.data
+    })
     return {
       tag, searchEntries
     }
