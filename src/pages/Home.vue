e451b1994ac208be4868aee687f61228bbb28a7c
trasta <trasta298@gmail.com>
2021-06-20 15:29:57 +0900

    hot

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="bg-primary flex-1">
     <div class="container mx-auto px-4">
-      <div class="text-3xl font-bold mt-16">
+      <div class="text-3xl font-bold mt-12">
+        Hot
+      </div>
+      <div class="grid lg:grid-cols-2 gap-5 mt-8 mb-16">
+        <entry-tile
+          v-for="(entry, index) in hotEntries"
+          :key="entry.url"
+          :entry="entry"
+          :index="index"
+          :change-bookmarks="changeBookmarksHotEntries"
+        />
+      </div>
+      <div class="text-3xl font-bold mt-12">
         New
       </div>
       <div class="grid lg:grid-cols-2 gap-5 mt-8 mb-16">
@@ -21,6 +33,7 @@
 import { defineComponent, ref, watchEffect } from 'vue'
 import EntryTile from '/@/components/EntryTile.vue'
 import apis, {EntryDetail} from '/@/lib/apis'
+
 export default defineComponent({
   name: 'Home',
   components: {
@@ -28,16 +41,23 @@ export default defineComponent({
   },
   setup() {
     const newEntries = ref<EntryDetail[]>([])
-    const changeBookmarks = (index: number) => {
+    const hotEntries = ref<EntryDetail[]>([])
+    const changeBookmarksNewEntries = (index: number) => {
       newEntries.value[index].count += newEntries.value[index].isBookmark? -1: 1
       newEntries.value[index].isBookmark = !newEntries.value[index].isBookmark
+    }
+    const changeBookmarksHotEntries = (index: number) => {
+      hotEntries.value[index].count += hotEntries.value[index].isBookmark? -1: 1
+      hotEntries.value[index].isBookmark = !hotEntries.value[index].isBookmark
     }
     watchEffect(async () => {
       const res = await apis.getEntry()
       newEntries.value = res.data
+      const res2 = await apis.getHotEntry()
+      hotEntries.value = res2.data
     })
     return {
-      newEntries, changeBookmarks
+      newEntries, hotEntries, changeBookmarksNewEntries, changeBookmarksHotEntries
     }
   }
 })
