ed58d7e385238ce69b70b155d20e0a3ce4a479af
trasta <trasta298@gmail.com>
2021-06-24 00:46:35 +0900

    pwa (できず)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
@@ -20,6 +21,22 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    WindiCSS()
+    WindiCSS(),
+    VitePWA({
+      manifest: {
+        lang: 'ja',
+        name: 'linQ',
+        short_name: 'linQ',
+        background_color: '#E5F6EF',
+        theme_color: '#0BB870',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 })
